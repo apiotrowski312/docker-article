1f74792898074cf2bcffdc2dbdeb78d61ff309e0
Aleksander Piotrowski <apiotrowski312@gmail.com>
2019-07-31 21:33:09 +0200

    Step 4: add postgres

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM alpine:3.10.1
 
 RUN apk add --no-cache \
-      python3=3.7.3-r0
+      build-base=0.5-r1 \
+      python3=3.7.3-r0 \
+      python3-dev=3.7.3-r0 \
+      postgresql-client=11.4-r0 \
+      postgresql-dev=11.4-r0
 
 
 COPY ./requirements.txt /
--- simple_app/simple_app/settings.py
+++ simple_app/simple_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'simple_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'test',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
